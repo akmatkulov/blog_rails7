f9de8d96b7b7437a2c3942436974be1da428ac3a
akmatkulov <akmatkulovs991@gmail.com>
2023-09-18 21:17:01 +0600

    Upd contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  # Return full title
+  def full_title(page_title = " ")
+    base_title = "Blog"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
